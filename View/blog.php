d75b7fe5fb3a90366324dcf690f742cbcc2c719c
lahfeuibovnwubvweioub <2004.nico.bauer@gmail.com>
2024-11-04 14:52:36 +0100

    -

--- View/blog.php
+++ View/blog.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Car.Blog</title>
+    <title>AutoInsider</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- Font awesome icon -->
     <link
