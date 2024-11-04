ae1a3ded46b49a0250ebf5a38a0dd96b446475f0
spaiK111 <maks.bogachenkov@gmail.com>
2024-11-04 14:21:02 +0100

    update der DB - Strukturen

--- Controller/api-website-dhbw/models/Blog.js
+++ Controller/api-website-dhbw/models/Blog.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
 const BlogPostSchema = new mongoose.Schema({
+    uid: { type: Number, autoIncrement: true },
     heading: {type: String, required: true},
     short_dsc: {type: String, required: true},
     long_dsc: {type: String, required: true},
     image: {type: String, required: true},
+    likes: {type: Number, default: 0},
+    created_By: {type: String, default: "Unknown"},
+    created_At: { type: Date, default: Date.now },
+    changed_At: { type: Date, default: Date.now },
+    changed_By: {type: String, default: "Unknown"},
 });
 
 
--- Controller/api-website-dhbw/models/User.js
+++ Controller/api-website-dhbw/models/User.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    login: { type : String, unique : true, required : true },
-    password: { type: String, required : true },
+    uid: { type: Number, autoIncrement: true }, // Auto-Increment wird üblicherweise durch ein Plugin wie "mongoose-auto-increment" verwaltet
+    login: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    admin: { type: Boolean, default: false },
+    LastName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    profile: { type: String, default: "" },
+    verified: { type: Boolean, default: true },
+    created_At: { type: Date, default: Date.now },
+    changed_At: { type: Date, default: Date.now }
 });
+
+// Hier kannst du ein Auto-Increment Plugin hinzufügen, wenn benötigt
 
 const User = mongoose.model('users', UserSchema);
 
