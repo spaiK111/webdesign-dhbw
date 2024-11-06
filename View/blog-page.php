deadfd8b55e470f7319ad3bdf78d34cfefbf0fa7
Duc2002 <nguyenvietduc311@gmail.com>
2024-11-06 18:56:39 +0100

    blog-page update v1

---
+++ View/blog-page.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/blog/blog-page.css">
+</head>
+<body>
+    <header>
+        <div class="navigation-bar">
+            <?php include "navbar.php"; ?>
+        </div>
+    </header>
+    <main>
+        <div class="blog-page-container">
+            <!--Image Part-->
+            <div class= "blog-page-image">
+                <img src="assets/blog/images/cars-blogpage-bg4.jpg">
+            </div>
+            <!--Text Part-->
+            <div class ="blog-page-text-container">
+
+                <div class="blog-page-heading">
+                    <h2>Automobilbranche tot?!</h2>
+                </div>
+
+                <div class="blog-page-short-description">
+                    <span>Automobilbranche Gewinneinbruch über 50%, bald das Ende?!</span>
+                </div>
+
+                <div class="blog-page-long-description">
+                    <p>kskdjkjdskjdksjksdjksjdsakjdskjkdsajkdjsakdjskajdksjkasjdskajk</p>
+                </div>
+
+            </div>
+
+        </div>
+        
+        
+
+
+    </main>
+    
+    <footer>
+        <div class="footer">
+            <?php include "footer.php"; ?>
+        </div>
+    </footer>
+</body>
+</html>
