fcd484c43da256d0062fc6be374146aba2bb7008
Spaik111 <maks.boga02@gmail.com>
2024-11-08 00:10:27 +0100

    test v5

--- View/assets/home/php/createDynamicOptions.php
+++ View/assets/home/php/createDynamicOptions.php
@@ -1,6 +1,6 @@
 <?php
 // Define your API endpoint
-$apiUrl = 'http://localhost:5000/api/posts/makeOptions';
+$apiUrl = 'http://localhost:5000/api/posts/';
 $response = file_get_contents($apiUrl);
 $makes = json_decode($response, true);
 
@@ -10,15 +10,21 @@ $topMakes = ['BMW', 'Audi', 'Mercedes-Benz'];
 // Check if $top is set to true
 if (isset($top) && $top === true) {
     echo "<!-- Display only top makes -->";
+    $displayedMakes = [];
     foreach ($makes as $makeOption) {
-        if (in_array($makeOption['make'], $topMakes)) {
+        if (in_array($makeOption['make'], $topMakes) && !isset($displayedMakes[$makeOption['make']])) {
             echo '<option value="' . htmlspecialchars($makeOption['make']) . '">' . htmlspecialchars($makeOption['make']) . '</option>';
+            $displayedMakes[$makeOption['make']] = true;
         }
     }
 } else {
     echo "<!-- Display all makes -->";
+    $displayedMakes = [];
     foreach ($makes as $makeOption) {
-        echo '<option value="' . htmlspecialchars($makeOption['make']) . '">' . htmlspecialchars($makeOption['make']) . '</option>';
+        if (!isset($displayedMakes[$makeOption['make']])) {
+            echo '<option value="' . htmlspecialchars($makeOption['make']) . '">' . htmlspecialchars($makeOption['make']) . '</option>';
+            $displayedMakes[$makeOption['make']] = true;
+        }
     }
 }
 ?>
