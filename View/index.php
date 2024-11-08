ac24933f2faa5fb70eb843cb8851a24b70e7b174
Spaik111 <maks.boga02@gmail.com>
2024-11-08 02:05:03 +0100

    Leistungssuche FIX auslagern

--- View/index.php
+++ View/index.php
@@ -196,6 +196,50 @@ $apiUrl = "http://localhost:5000/api/posts/getUserData/?login=$login&hashedPassw
         </div>
 
 
+        <div class="search-container-item" id ="search-container-leistungsbereich">
+          <label>Modelljahr</label>
+          <div class="search-selection-item" id ="leistungsbereich-selection" >
+            <input class="search-selection" list="modelljahr1" id="year1" name="year1" placeholder="Beliebig" type="number" disabled/>
+            <datalist id="modelljahr1">
+              <option value="2000"></option>
+              <option value="2001"></option>
+              <option value="2002"></option>
+              <option value="2003"></option>
+              <option value="2004"></option>
+              <option value="2005"></option>
+              <option value="2006"></option>
+              <option value="2007"></option>
+              <option value="2008"></option>
+              <option value="2009"></option>
+              <option value="2010"></option>
+            </datalist>
+
+            <span class="search-selection-span">bis</span>
+
+            <input class="search-selection" list="modelljahr2" id="year2" name="year2" placeholder="Beliebig" type="number" disabled/>
+            <datalist id="modelljahr2">
+              <option value="2011"></option>
+              <option value="2012"></option>
+              <option value="2013"></option>
+              <option value="2014"></option>
+              <option value="2015"></option>
+              <option value="2016"></option>
+              <option value="2017"></option>
+              <option value="2018"></option>
+              <option value="2019"></option>
+              <option value="2020"></option>
+              <option value="2021"></option>
+              <option value="2022"></option>
+              <option value="2023"></option>
+              <option value="2024"></option>
+            </datalist>
+
+            <div class="search-box">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="292.362px" height="292.362px" viewBox="0 0 292.362 292.362">
+                    <path d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424 C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428 s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z"/>
+                </svg>
+            </div>
+        </div>
 
 
         </div>
